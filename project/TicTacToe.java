dd1fd36f5f6b58dfcfd5eda9376bfb4d9d5d8bef
Chris Marotta <marottachristopher@gmail.com>
2017-11-26 19:58:19 -0500

    fixed TicTacToe project

--- project/TicTacToe.java
+++ project/TicTacToe.java
@@ -10,6 +10,7 @@ public class TicTacToe
     //PIV
     private static final int ROWS = 3;
     private static final int COLUMNS = 3;
+    private static final int REQUIRED_MATCHES = 3;
     private String[][] board;
     
     /**
@@ -31,6 +32,124 @@ public class TicTacToe
         if(board[i][j].equals(" ")){
             board[i][j] = player;
         }
+    }
+    /**
+     * checks to see if a space is unoccupied.
+     */
+    public boolean spaceAvailable(int row, int column){
+        if(board[row][column].equals(" ")){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    /**
+     * Clears board
+     */
+    public void boardClear(){
+        for(int i=0;i<board.length;i++){
+            for(int x=0;x<board[0].length;x++){
+                board[i][x]=" ";
+            }
+        }
+    }
+    /**
+     * Checks for any winning combinations at the end of each turn.
+     */
+    public boolean spaceCheck(String player){
+        String charOfCurrSpace = "";
+        int matches = 0;
+        boolean matching = true;
+        while(matches<=REQUIRED_MATCHES){
+            //Checks all rows for a ttt
+            for(int i = 0; i<ROWS; i++){
+                matches=0;
+                matching = true;
+                while(matching){
+                    for(int x = 0; x<COLUMNS; x++){
+                        charOfCurrSpace = board[i][x];
+                        if(charOfCurrSpace.equals(player)){
+                            matches++;
+                            if(matches==REQUIRED_MATCHES){
+                                System.out.println(player + " WINS!");
+                                return true;
+                            }
+                        }
+                        else{
+                            matching = false;
+                        }
+                    }
+                    matching = false;
+                }
+            }
+            System.out.println("l");
+            
+            //checks all columns for a ttt
+            for(int y=0; y<COLUMNS; y++){
+                matches=0;
+                matching = true;
+                while(matching){
+                    for (int z=0; z<ROWS; z++){
+                        charOfCurrSpace = board[z][y];
+                        if(charOfCurrSpace.equals(player)){
+                            matches++;
+                            if(matches==REQUIRED_MATCHES){
+                                System.out.println(player + " WINS!");
+                                return true;
+                            }
+                        }
+                        else{
+                            matching = false;
+                        }
+                    }
+                    matching = false;
+                }
+            }
+                        System.out.println("l");
+            matching = true;
+            //checks for diagonal left to right
+            matches=0;
+            while(matching){
+                for(int m=0; m<COLUMNS; m++){
+                    charOfCurrSpace = board[m][m];
+                    if(charOfCurrSpace.equals(player)){
+                        matches++;
+                        if(matches==REQUIRED_MATCHES){
+                            System.out.println(player + "WINS!");
+                            return true;
+                        }
+                    }
+                    else{
+                        matching = false;
+                    }
+                    matching = false;
+                }
+            }
+                        System.out.println("l");
+            matching = true;
+            //checks for diagonals right to left
+            matches=0;
+            while(matching){
+                for(int m=2; m<=0; m--){
+                    charOfCurrSpace = board[m][m];
+                    if(charOfCurrSpace.equals(player)){
+                        matches++;
+                        if(matches==REQUIRED_MATCHES){
+                            System.out.println(player + "WINS!");
+                            return true;
+                        }
+                    }
+                    else{
+                        matching = false;
+                    }
+                }
+                matching = false;
+            }
+            return false;
+        }
+        System.out.println(player + "WINS!");
+        return true;
     }
     /**
      * Returns a string, so ur board kan hav spme prionty boies and kan be shwn.
--- project/TicTacToePlayer.java
+++ project/TicTacToePlayer.java
@@ -11,26 +11,62 @@ public class TicTacToePlayer
         Scanner in = new Scanner(System.in);
         String player = "x";
         TicTacToe ttt = new TicTacToe();
+        int turns = 0;
         boolean done = false;
-        
+        boolean winners = false;
         while(!done){
-            System.out.println(ttt);
-            System.out.println("Row for player " + player + "(-1 to exit)");
-            int row = in.nextInt();
-            if(row<0){
-                done=true;
-            }
-            else{
-                System.out.println("Column for player " + player+": ");
-                int col = in.nextInt();
-                
-                ttt.set(row,col,player);
-                if(player.equals("x")){
-                    player="o";
+            while(!winners){
+                System.out.println(ttt);
+                System.out.println("Row for player " + player + " (-1 to exit)");
+                int row = in.nextInt();
+                if(row<0){
+                    done=true;
+                    winners = true;
+                    System.out.println("PLAYER " + player + " FOREFITS THE MATCH THEY LOOSE!");
                 }
                 else{
-                    player="x";
+                    System.out.println("Column for player " + player+": ");
+                    int col = in.nextInt();
+                
+                    if(ttt.spaceAvailable(row,col)){
+                        ttt.set(row,col,player);
+                        winners = (ttt.spaceCheck(player));
+                        done = (ttt.spaceCheck(player));
+                        if(player.equals("x")){
+                            player="o";
+                        }
+                        else{
+                            player="x";
+                        }
+                    }
+                    else{
+                        System.out.println("Space taken! Please try again!");
+                        turns--;
+                    }
+                }   
+                turns++;
+                if(turns==9){
+                    winners = true;
+                    
+                    if(ttt.spaceCheck(player)==true){
+                        
+                    }
+                    else{
+                        System.out.println("NOBODY WINS!");
+                    }
+                    turns=0;
                 }
+            }
+            System.out.println("Would you like to play again? Y/N");
+            String playAgain = in.next();
+            if(playAgain.equalsIgnoreCase("Y")){
+                winners = false;
+                done = false;
+                ttt.boardClear();
+                turns = 0;
+            }
+            else{
+                winners = true;
             }
         }
     }
