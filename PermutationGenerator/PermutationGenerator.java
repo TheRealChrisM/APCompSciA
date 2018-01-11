fd99a8672ab8ca92dff1d67981595c89b779a2f3
Chris Marotta <marottachristopher@gmail.com>
2018-01-11 08:29:51 -0500

    worked on permutationGenerator

---
+++ PermutationGenerator/PermutationGenerator.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class PermutationGenerator here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class PermutationGenerator
+{
+    String setWord;
+    
+    public PermutationGenerator(String aWord){
+        setWord = aWord;
+        
+    }
+    
+    public ArrayList<String> getPermutation(){
+       ArrayList<String> rs = new ArrayList<String>();
+       rs.add(setWord);
+       return rs; 
+    }
+    
+    public String generatePermutations(String wordIn){
+        
+        return 
+    }
+}
---
+++ PermutationGenerator/PermutationTester.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class PermutationTester here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class PermutationTester
+{
+    public static void main(String[] args){
+        PermutationGenerator gen = new PermutationGenerator("cat");
+        ArrayList<String> perms = gen.getPermutation();
+        
+        for(String s : perms){
+            System.out.println(s);
+        }
+
+    }
+}
